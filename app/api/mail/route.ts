38558e1fca7c77a9256dda5eb0efc6d8cd81e39d
Emmanuel <oketunbi.olufunke@gmail.com>
2024-07-04 18:57:59 +0100

    build issues

--- app/api/mail/route.ts
+++ app/api/mail/route.ts
@@ -11,13 +11,19 @@ import nodemailer from "nodemailer"
 })
 
 export async function POST(req:Request){
- try {
+  try {
   const { email, user } = await req.json()
 
-     // Use <h4 style="font-weight:700;">${user[0]?.id} <h4/> to login temporarily into the reset link sent
+   transporter.sendMail({
+      from: "loluwasimi54@gmail.com",
+      to:email,
+      subject: "Password Recovery",
+      html: `Hi ${user[0]?.name} ! Seems you forgot your password. <br> <br/>
       
-    //  Note: DONT USE ANY OTHER PASSWORD`
-  //  })
+      Use <h4 style="font-weight:700;">${user[0]?.id} <h4/> to login temporarily into the reset link sent
+      
+      Note: DONT USE ANY OTHER PASSWORD`
+    })
  } catch (err) {
   return {
     error: err
