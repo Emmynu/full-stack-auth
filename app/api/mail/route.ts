f32bcfee10c3205d428ca7a4d18f2ada7f73eaf3
Similoluwa Adelaja <118024691+Emmynu@users.noreply.github.com>
2024-07-04 14:01:15 +0100

    Update route.ts

--- app/api/mail/route.ts
+++ app/api/mail/route.ts
@@ -14,16 +14,10 @@ export async function POST(req:Request){
  try {
   const { email, user } = await req.json()
 
-   transporter.sendMail({
-      from: "loluwasimi54@gmail.com",
-      to:email,
-      subject: "Password Recovery",
-      html: `Hi ${user[0]?.name} ! Seems you forgot your password. <br> <br/>
+     // Use <h4 style="font-weight:700;">${user[0]?.id} <h4/> to login temporarily into the reset link sent
       
-      Use <h4 style="font-weight:700;">${user[0]?.id} <h4/> to login temporarily into the reset link sent
-      
-      Note: DONT USE ANY OTHER PASSWORD`
-    })
+    //  Note: DONT USE ANY OTHER PASSWORD`
+  //  })
  } catch (err) {
   return {
     error: err
